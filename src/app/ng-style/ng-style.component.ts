40020c224518f8d131f23bb89739f0a85cfaa938
Paulo Correa <paulo.correa@indt.org.br>
2019-09-03 16:37:30 -0400

    ngStyle added

--- src/app/ng-style/ng-style.component.ts
+++ src/app/ng-style/ng-style.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NgStyleComponent implements OnInit {
 
+  myFontSize = "10px";
+  myColor = "green";
+
   constructor() { }
 
   ngOnInit() {
