db487473287787e6987f0cdc5a4f4985aa3df427
Paulo <paulo.correa@indt.org.br>
2019-11-17 15:59:04 -0400

    fix ngswitch class padding

--- src/app/ng-style/ng-style.component.ts
+++ src/app/ng-style/ng-style.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class NgStyleComponent implements OnInit {
 
   myFontSize = "10px";
-  myColor = "green";
+  myColor = "red";
+  myColor1="red";
 
   constructor() { }
 
